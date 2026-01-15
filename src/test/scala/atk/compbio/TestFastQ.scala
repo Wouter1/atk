40296db2031c8cebbf646d620f58c6a3873a9c6c
Wouter <nomail@hotmail.com>
2026-01-15 17:04:42 +0100

    '#2 fix pom to also compile test resources. fix path to use the propertest file.

--- src/test/scala/atk/compbio/TestFastQ.scala
+++ src/test/scala/atk/compbio/TestFastQ.scala
@@ -12,11 +12,11 @@ object TestFastQ {
 class TestFastQ extends AnyFunSuite {
 
   test("test fq") {
-    for (entry <- FastQFile("test/test.fastq")) {
+    for (entry <- FastQFile("src/test/resources/test.fastq")) {
       println(entry)
     }
 
-    FastQFile("test/test.fastq").filter(_.name.contains("1")).toList.map(println(_))
+    FastQFile("src/test/resources/test.fastq").filter(_.name.contains("1")).toList.map(println(_))
   }
 
 }
