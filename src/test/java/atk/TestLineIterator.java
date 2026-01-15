f83db28fa92f6dcd91556913aef62a30ce3dc352
Wouter <nomail@hotmail.com>
2026-01-15 15:50:41 +0100

    '#2 reconstructed test file and .gz file to re-enable test

--- src/test/java/atk/TestLineIterator.java
+++ src/test/java/atk/TestLineIterator.java
@@ -10,19 +10,23 @@ import junit.framework.Assert;
 import be.abeel.io.LineIterator;
 
 public class TestLineIterator {
+	private final static String TEXTFILE = "src/test/resources/COPYING";
+	private final static String GZTFILE = "src/test/resources/COPYING.gz";
+	
 
 	@Test
 	public void testLineIterator(){
-		LineIterator it=new LineIterator("COPYING");
-		LineIterator it2=new LineIterator("COPYING.gz");
+		LineIterator it=new LineIterator(TEXTFILE);
+		LineIterator it2=new LineIterator(GZTFILE);
 		while(it.hasNext()){
 			Assert.assertEquals(it.next(), it2.next());
 		}
 	}
 	@Test
 	public void testLineIterator2(){
-		LineIterator it=new LineIterator("COPYING");
-		LineIterator it2=new LineIterator(new File("COPYING.gz"));
+		LineIterator it=new LineIterator(TEXTFILE);
+		LineIterator it2=new LineIterator(GZTFILE);
+
 		while(it.hasNext()){
 			Assert.assertEquals(it.next(), it2.next());
 		}
@@ -30,10 +34,10 @@ public class TestLineIterator {
 	
 	@Test
 	public void testLineIterator3(){
-		LineIterator it=new LineIterator("COPYING");
+		LineIterator it=new LineIterator(TEXTFILE);
 		LineIterator it2=null;
 		try {
-			it2 = new LineIterator(new FileInputStream("COPYING.gz"));
+			it2 = new LineIterator(new FileInputStream(GZTFILE));
 		} catch (FileNotFoundException e) {
 			Assert.fail();
 		}
